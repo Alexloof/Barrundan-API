348365d9de5fd8036e623a082130f3cffc6440a9
spixooze <s_simon_s@hotmail.com>
2017-10-05 19:33:04 +0200

    Mongoose Warning fixed. 404 error send object with message fixed

--- src/app.js
+++ src/app.js
@@ -1,24 +1,28 @@
-import express, {Router} from 'express'
+import express, { Router } from 'express'
 import logger from 'morgan'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import passport from 'passport'
 import config from './config'
-import {jwt_strategy} from './config/jwt'
+import { jwt_strategy } from './config/jwt'
 import routes from './routes/routes'
 
 const app = express()
 
 // Add jwt stratergy
-passport.use(jwt_strategy);
-app.use(passport.initialize());
+passport.use(jwt_strategy)
+app.use(passport.initialize())
 
-const router = new Router();
+const router = new Router()
 
-// Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
+// Mongoose's built in promise library is deprecated, replaced with ES2015 Promise
 mongoose.Promise = global.Promise
 
-mongoose.connect(config.db_url)
+mongoose.connect(config.db_url, {
+  keepAlive: true,
+  reconnectTries: 4,
+  useMongoClient: true
+})
 
 mongoose.connection
   .once('open', () => console.log('Connected to MongoLab instance.'))
@@ -40,33 +44,21 @@ app.use(bodyParser.json())
 // Routes
 routes(router)
 
-app.use(router);
+app.use(router)
 
-
+// Handle Errors
 app.use((err, req, res, next) => {
-    /* Handle Errors */
-    res.status(err.status).send({
-        error: err.status,
-        message: err.message
-    });
-
-});
-
-app.use(function (req, res, next) {
-    res.status(404).send('These are not the droids you are looking for')
+  res.status(err.status).send({
+    error: err.status,
+    message: err.message
+  })
 })
-
-// CORS setup?
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*')
-//   res.header(
-//     'Access-Control-Allow-Headers',
-//     'Origin, X-Requested-With, Content-Type, Accept'>
-//   )
-//   if (req.method === 'Options') {
-//     res.header('Access-Control-Allow-Methods', 'PUT, POST, DELETE')
-//     return res.status(200).json({})
-//   }
-// })
+// Handle 404
+app.use(function(req, res, next) {
+  res.status(404).send({
+    error: 404,
+    message: 'These are not the droids you are looking for'
+  })
+})
 
 export default app
