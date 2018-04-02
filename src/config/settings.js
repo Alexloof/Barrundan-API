2bf6a5420d2a9e2db8ca28391e431249d7f167ac
spixooze <simon.code@outlook.com>
2018-04-02 12:56:44 +0200

    remove keys

--- src/config/settings.js
+++
@@ -1,4 +0,0 @@
-export const GOOGLE_PLACE_KEY = 'AIzaSyBW2usWTrHIZR02D3-qWj0oPbaW_ivcRmk'
-export const DB_URL =
-  'mongodb://alexfey:barrundan123@ds151544.mlab.com:51544/barrundan-db'
-export const JWT_SECRET = '0tLJ6niJpI6e6mHqZPsPiCjMX1GYcm'
